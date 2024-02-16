db09644f45ece200ef21c10bc1ed19732dc367cd
Steven-Low <2002lowsy@gmail.com>
2024-02-16 21:33:06 +0800

    halo

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello, World!'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')  # Important for Render deployment
